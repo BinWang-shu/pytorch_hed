f80b586d3a01a5503e4e24620a387f93467c1672
BinWang-shu <352186428@qq.com>
2018-01-16 14:58:31 +0800

    change load pretrain model

---
+++ utils/our_vgg.py
@@ -0,0 +1,68 @@
+import torch.nn as nn
+import math
+import torch
+import torch.nn.functional as F
+
+class vgg(nn.Module):
+    def __init__(self):
+        super(vgg, self).__init__()
+        self.conv1 = nn.Sequential(
+            # conv1
+            nn.Conv2d(3, 64, 3, padding=35),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            
+        )
+        self.conv2 = nn.Sequential(
+            # conv2
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/2
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            
+        )
+        self.conv3 = nn.Sequential(
+            # conv3
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/4
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            
+        )
+        self.conv4 = nn.Sequential(
+            # conv4
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/8
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            
+        )
+        self.conv5 = nn.Sequential(
+            # conv5
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/16
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+         
+
+    def forward(self, x):
+
+        conv1 = self.conv1(x)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+        conv5 = self.conv5(conv4)
+
+        return conv5
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.misc import imread, imresize, imsave
 import torch
+import copy
+from our_vgg import vgg
 
 
 def is_image_file(filename):
@@ -10,3 +12,26 @@ def deprocess(img):
     img = img.add_(1).div_(2)
 
     return img
+
+def transfer_weights(model_from, model_to):
+    wf = copy.deepcopy(model_from.state_dict())
+    wt = model_to.state_dict()
+    for k in wt.keys():
+        if not k in wf:
+            wf[k] = wt[k]
+    model_to.load_state_dict(wf)
+
+def convert_vgg(vgg16):
+	net = vgg()
+
+	vgg_items = net.state_dict().items()
+	vgg16_items = vgg16.items()
+
+	pretrain_model = {}
+	j = 0
+	for k, v in net.state_dict().iteritems():
+	    v = vgg16_items[j][1]
+	    k = vgg_items[j][0]
+	    pretrain_model[k] = v
+	    j += 1
+	return pretrain_model
